951fcbad0b3fac6a32c2161e7f9717ca36ab4604
Egor Fedyaev <georgefed@yandex-team.ru>
2023-06-30 15:36:35 +0300

    Stage 4: Adds deeplinking support

--- lib/navigation/router_delegate.dart
+++ lib/navigation/router_delegate.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
+
 import 'package:deeplinks_app/ui/cart_screen.dart';
 import 'package:deeplinks_app/ui/item_details_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:uni_links/uni_links.dart';
 
 import '../domain/models/routes.dart';
 import '../ui/item_list_screen.dart';
@@ -74,6 +78,65 @@ class MyRouterDelegate extends RouterDelegate<NavigationState>
         return true;
       },
     );
+  }
+
+  StreamSubscription<String?>? _linkStreamSubscription;
+
+  Future<void> initLinkStream() async {
+    await initPlatformState();
+
+    _linkStreamSubscription = linkStream.listen((String? link) {
+      _handleLink(link);
+    });
+  }
+
+  Future<void> initPlatformState() async {
+    try {
+      await Future.wait<void>([
+        getInitialLink(),
+        initLinkStream(),
+      ]);
+      // ignore: empty_catches
+    } on PlatformException {}
+  }
+
+  @override
+  void dispose() {
+    _linkStreamSubscription?.cancel(); // Cancel the link stream subscription
+    super.dispose();
+  }
+
+  Future<void> parseRoute() async {
+    final initialLink = await getInitialLink();
+    _handleLink(initialLink);
+
+    linkStream.listen((String? link) {
+      _handleLink(link);
+    });
+  }
+
+  void _handleLink(String? link) {
+    if (link != null) {
+      // Parse the deep link and extract the necessary information
+      final uri = Uri.parse(link);
+      if (uri.pathSegments.isEmpty) {
+        // Handle the root deep link
+        _selectedItemId = null;
+        _isCart = false;
+      } else if (uri.pathSegments.first == Routes.item) {
+        final itemId = uri.pathSegments.length > 1 ? uri.pathSegments[1] : null;
+        _selectedItemId = itemId;
+        _isCart = false;
+      } else if (uri.pathSegments.first == Routes.cart) {
+        _selectedItemId = null;
+        _isCart = true;
+      }
+    } else {
+      _selectedItemId = null;
+      _isCart = false;
+    }
+
+    notifyListeners();
   }
 
   @override
